e4c4820506d077dfef75c4e5eb8e82289e150f2a
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-05-03 21:04:14 -0500

    Start Project

---
+++ DjangoShell.py
@@ -0,0 +1,19 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza
+
+pizzas = Pizza.objects.all()
+
+for p in pizzas:
+    print(p.id, ' ', p.pizza_name)
+
+p = Pizza.objects.get(id=1)
+
+toppings = p.topping_set.all()
+
+for t in toppings:
+    print(t.pizza_name)
